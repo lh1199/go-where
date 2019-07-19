76d6170ba02131573903626215e6419258511ee5
leihao <991454866@qq.com>
2019-07-19 14:48:08 +0800

    轮播图

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <i class="iconfont">&#xe624;</i>
+    </div>
+    <div class="header-input">
+      <i class="iconfont">&#xe632;</i>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <i class="iconfont" style="font-size: .12rem">&#xe64a;</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header
+  display: flex
+  line-height: .86rem
+  background: #00bcd4
+  color: #fff
+  .header-left
+    width: .64rem
+    float: left
+  .header-input
+    flex: 1
+    margin-top: .12rem
+    margin-left: .2rem
+    padding-left: .1rem
+    line-height: .64rem
+    height: .64rem
+    background: #fff
+    border-radius: .1rem
+    color: #ccc
+    text-align: left
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align: center
+</style>
+
+
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" style="touch-action: none">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img
+          class="swiper-img"
+          :src="item.imgUrl"
+        />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '01',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg'
+      }, {
+        id: '02',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/bc62e131afb380e98e4beac33e751c6c.jpg_750x200_2e248116.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff !important
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 26.67%
+  background: #eee
+  .swiper-img
+    width: 100%;
+</style>
+
+
